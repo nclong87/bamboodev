2e9b80e40c37f2d1420c52613ea02dfe0d22472d
imleeu <imleeu@fd54f6a5-de74-b557-b740-1972abe2fde4>
2013-05-07 15:31:25 +0000

    update for a few issues

--- branches/noithat/wp-content/themes/bamboodev/page-index.php
+++ branches/noithat/wp-content/themes/bamboodev/page-index.php
@@ -18,15 +18,17 @@ $pagenum = isset($_GET['pagenum'])?$_GET['pagenum']:1;
 <?php if(function_exists('wp_content_slider')) { wp_content_slider(); } ?>
 <div style="float: left; width: 100%; margin-top: 5px;">
 	<div id="center">
-		<h3>Sản Phẩm Mới</h3>
+		<h3>Nội Thất Văn Phòng</h3>
 		<?php
-		$posts = query_posts( array('post_type' => 'product', 'orderby' => 'created', 'order' => 'DESC','posts_per_page' => 30,'paged' => 1));
-		foreach($posts as $post) {
-			$image = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID),'thumbnail', 'single-post-thumbnail' );
+		
+		//$posts = query_posts( array('post_type' => 'product', 'orderby' => 'created', 'order' => 'DESC','posts_per_page' => 30,'paged' => 1));
+		$categories = get_categories( array('orderby' => 'name', 'order' => 'ASC', 'parent'=> 4));
+		foreach($categories as $category) {
+		//	$image = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID),'thumbnail', 'single-post-thumbnail' );
 		?>
-		<a class="product_item" href="<?php echo get_permalink( $post->ID );?>" title="<?php echo $post->post_title?>">
-			<span class="title"><?php echo $post->post_title?></span>
-			<img width="150" height="150" alt="<?php echo $post->post_title?>" src="<?php echo $image[0]?>">
+		<a class="product_item" href="<?php echo get_category_link( $category->term_id );?>" title="<?php echo $category->name?>">
+			<span class="title"><?php echo $category->name?></span>
+			<img width="150" height="150" alt="<?php echo $category->name?>" src="<?php //echo $image[0]?>">
 		</a>
 		<?php
 		}
